1919b19104452c0f0139890e860285035dd14ff3
thomascombe <thomascombe42@gmail.com>
2021-04-23 08:29:16 +0200

    Update package name

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -29,7 +29,7 @@ class TestCase extends Orchestra
         config()->set('database.default', 'testing');
 
         /*
-        include_once __DIR__.'/../database/migrations/create_laravel_observer_attributes_table.php.stub';
+        include_once __DIR__.'/../database/migrations/create_laravel-observer-attributes_table.php.stub';
         (new \CreatePackageTable())->up();
         */
     }
